2cc609d2c3730fc3e015cd74aefbd15c40e8f277
a4koshin <mankajr11@gmail.com>
2026-01-09 22:32:23 +0300

    Navbar data is added

---
+++ lib/navigation.ts
@@ -0,0 +1,28 @@
+interface Navbar {
+  id: number;
+  name: string;
+  href: string;
+}
+
+export const navbar: Navbar[] = [
+  {
+    id: 0,
+    name: "Home ",
+    href: "/",
+  },
+  {
+    id: 1,
+    name: "About Us",
+    href: "/about ",
+  },
+  {
+    id: 2,
+    name: "Our Services",
+    href: "/ourservice",
+  },
+  {
+    id: 3,
+    name: "Contact Us",
+    href: "/contact ",
+  },
+];
